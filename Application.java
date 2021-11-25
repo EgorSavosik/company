89ca48c7a96098c372b66f9c4808305efec0675d
EgorSavosik <95018795+EgorSavosik@users.noreply.github.com>
2021-11-25 11:20:37 +0300

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,14 @@
+package college.of.electronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class);
+    }
+}
